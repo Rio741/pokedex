7673b7dfbf63a57f87d8f2b687f8baf53ffeae65
Rio Stenger <164682853+Rio741@users.noreply.github.com>
2025-05-15 23:37:04 +0700

    remove comments

--- scripts/chart.js
+++ scripts/chart.js
@@ -1,10 +1,10 @@
 const customColors = [
-  'rgb(255, 99, 132)', // Rot
-  'rgb(54, 162, 235)', // Blau
-  'rgb(255, 205, 86)', // Gelb
-  'rgb(75, 192, 192)', // Grün
-  'rgb(153, 102, 255)', // Lila
-  'rgb(255, 159, 64)', // Orange
+  'rgb(255, 99, 132)',
+  'rgb(54, 162, 235)',
+  'rgb(255, 205, 86)',
+  'rgb(75, 192, 192)',
+  'rgb(153, 102, 255)',
+  'rgb(255, 159, 64)',
 ];
 
 function loadChart(pokemonData) {
--- scripts/script.js
+++ scripts/script.js
@@ -24,30 +24,46 @@ const typeColors = {
   fairy: "rgba(238, 153, 172, 0.8)",
 };
 
+/**
+ * Initializes the application by fetching the list of all Pokémon,
+ * pushing them into an array, and fetching the Pokémon data for display.
+ */
 async function init() {
- await fetchHolePokemonList();
- pushAllPokemonsToArray();
- fetchPokemons() //Pokemon-Mainpage
+  await fetchHolePokemonList();
+  pushAllPokemonsToArray();
+  fetchPokemons();
 }
 
+/**
+ * Fetches the list of all Pokémon.
+ * @returns {Promise<void>}
+ */
 async function fetchHolePokemonList() {
-  let url = 'https://pokeapi.co/api/v2/pokemon?limit=1500&offset=0.';
+  let url = "https://pokeapi.co/api/v2/pokemon?limit=1500&offset=0.";
   let response = await fetch(url);
   let amountOfPokemon = await response.json();
-  let amountOfAllPokemon = await amountOfPokemon['count'];
+  let amountOfAllPokemon = await amountOfPokemon["count"];
 }
 
+/**
+ * Pushes all Pokémon to the global array 'allPokemon'.
+ * @returns {Promise<void>}
+ */
 async function pushAllPokemonsToArray() {
   for (let i = 1; i < 1026; i++) {
-      let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-      let response = await fetch(url);
-      let currentPokemon = await response.json();
-      allPokemon.push(currentPokemon);
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+    let response = await fetch(url);
+    let currentPokemon = await response.json();
+    allPokemon.push(currentPokemon);
   }
 }
 
+/**
+ * Fetches Pokémon data based on the current count value.
+ * @returns {Promise<void>}
+ */
 async function fetchPokemons() {
-  if (isLoading) return; // Wenn bereits eine Anfrage im Gange ist, beende die Funktion
+  if (isLoading) return;
   isLoading = true;
   const MAIN_URL = `https://pokeapi.co/api/v2/pokemon/?offset=${COUNT_VALUE}`;
   let response = await fetch(MAIN_URL);
@@ -56,9 +72,14 @@ async function fetchPokemons() {
   for (let i = 0; i < pokemonList.length; i++) {
     await getPokemonInfos(pokemonList[i].url);
   }
-    isLoading = false;
+  isLoading = false;
 }
 
+/**
+ * Fetches Pokémon information from a given URL and renders it.
+ * @param {string} url - The URL of the Pokémon data.
+ * @returns {Promise<void>}
+ */
 async function getPokemonInfos(url) {
   let response = await fetch(url);
   let pokemon = await response.json();
@@ -67,9 +88,20 @@ async function getPokemonInfos(url) {
   let secondType = checkSecondPokeType(pokemon);
   let displayValue = secondType ? "inline" : "none";
   let pokedek = document.getElementById("pokedek");
-  pokedek.innerHTML += renderPokemon(pokemon, formattedId, secondType, displayValue, pokemonImage);//im templates-ordner
+  pokedek.innerHTML += renderPokemon(
+    pokemon,
+    formattedId,
+    secondType,
+    displayValue,
+    pokemonImage
+  );
 }
 
+/**
+ * Fetches and renders Pokémon data for a specific Pokémon ID.
+ * @param {number} pokemonId - The ID of the Pokémon to fetch.
+ * @returns {Promise<void>}
+ */
 async function fetchCardPokemon(pokemonId) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
   let response = await fetch(url);
@@ -77,49 +109,90 @@ async function fetchCardPokemon(pokemonId) {
   getPokemonCardInfos(pokemonData);
 }
 
+/**
+ * Retrieves Pokémon card information and updates the card display.
+ * @param {Object} pokemonData - The data of the Pokémon to display.
+ * @returns {void}
+ */
 function getPokemonCardInfos(pokemonData) {
   const pokeCard = document.getElementById("pokecard");
-  setPokemonCard(pokeCard); // Die Pokecard anzeigen
+  setPokemonCard(pokeCard);
   const primaryType = pokemonData.types[0].type.name;
-  const backgroundColor = typeColors[primaryType]
+  const backgroundColor = typeColors[primaryType];
   const formattedId = String(pokemonData.id).padStart(3, "0");
   let secondType = checkSecondPokeType(pokemonData);
   let displayValue = secondType ? "inline" : "none";
   let pokemonImage = pokemonData.sprites.other.dream_world.front_default;
-  pokeCard.innerHTML = renderPokemonCard(pokemonData, backgroundColor, formattedId, secondType, displayValue, pokemonImage);//im templates-ordner
+  pokeCard.innerHTML = renderPokemonCard(
+    pokemonData,
+    backgroundColor,
+    formattedId,
+    secondType,
+    displayValue,
+    pokemonImage
+  );
   pokemonStatusValues(pokemonData);
 }
 
+/**
+ * Loads more Pokémon by increasing the count value and fetching Pokémon data.
+ * @returns {void}
+ */
 function loadMorePokemons() {
   COUNT_VALUE += 20;
   fetchPokemons();
 }
 
-function setPokemonCard(pokeCard){
+/**
+ * Displays the Pokémon card and darkens the background.
+ * @param {HTMLElement} pokeCard - The HTML element representing the Pokémon card.
+ * @returns {void}
+ */
+function setPokemonCard(pokeCard) {
   pokeCard.style.display = "flex";
   document.getElementById("blackscreen").style.display = "flex";
   document.body.style.overflow = "hidden";
 }
 
+/**
+ * Closes the Pokémon card and restores the background.
+ * @returns {void}
+ */
 function closePokemonCard() {
   document.getElementById("blackscreen").style.display = "none";
   document.getElementById("pokecard").style.display = "none";
   document.body.style.overflow = "auto";
 }
 
-function clickLeft(pokemonData){
+/**
+ * Handles the click event for moving left in the Pokémon evolution chain.
+ * @param {number} pokemonData - The ID of the Pokémon.
+ * @returns {void}
+ */
+function clickLeft(pokemonData) {
   if (pokemonData > 0) {
     fetchCardPokemon(pokemonData);
   }
 }
 
-function clickRight(pokemonData){
+/**
+ * Handles the click event for moving right in the Pokémon evolution chain.
+ * @param {number} pokemonData - The ID of the Pokémon.
+ * @returns {void}
+ */
+function clickRight(pokemonData) {
   fetchCardPokemon(pokemonData);
 }
 
-//Functions Evolution
+/**
+ * Fetches the evolution chain of a Pokémon and creates its evolution chart.
+ * @param {number} pokemonId - The ID of the Pokémon.
+ * @returns {void}
+ */
 async function fetchEvolution(pokemonId) {
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`);
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon-species/${pokemonId}/`
+  );
   const data = await response.json();
   const evolutionChainUrl = data.evolution_chain.url;
   const evolutionChainResponse = await fetch(evolutionChainUrl);
@@ -127,10 +200,16 @@ async function fetchEvolution(pokemonId) {
   createEvolution(evolutionChainData.chain);
 }
 
+/**
+ * Creates the Pokémon evolution chain based on the chain data.
+ * @param {Object} chain - The evolution chain data.
+ * @returns {void}
+ */
 async function createEvolution(chain) {
-  const categoryContent = document.getElementById('category-content');
-  categoryContent.innerHTML = '<div><h2 class="headline-evolution">Evolution-Chain</h2><div id="evolutionDiv"></div></div>';
-  const evolutionDiv = document.getElementById('evolutionDiv');
+  const categoryContent = document.getElementById("category-content");
+  categoryContent.innerHTML =
+    '<div><h2 class="headline-evolution">Evolution-Chain</h2><div id="evolutionDiv"></div></div>';
+  const evolutionDiv = document.getElementById("evolutionDiv");
   while (chain) {
     const pokemonName = chain.species.name;
     const formattedName = formatPokemonName(chain.species);
@@ -141,14 +220,27 @@ async function createEvolution(chain) {
   }
 }
 
+/**
+ * Fetches evolution data for a Pokémon.
+ * @param {string} pokemonName - The name of the Pokémon.
+ * @returns {Object} - The Pokémon data.
+ */
 async function fetchEvolutionData(pokemonName) {
-  const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`);
+  const pokemonResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`
+  );
   return await pokemonResponse.json();
 }
 
+/**
+ * Creates an evolution div for a Pokémon.
+ * @param {string} formattedName - The formatted name of the Pokémon.
+ * @param {Object} pokemonData - The data of the Pokémon.
+ * @returns {HTMLElement} - The HTML element representing the Pokémon div.
+ */
 function createEvolutionDiv(formattedName, pokemonData) {
   const pokemonImage = pokemonData.sprites.other.dream_world.front_default;
-  const pokemonDiv = document.createElement('div');
+  const pokemonDiv = document.createElement("div");
   pokemonDiv.innerHTML = `
     <h3>${formattedName}</h3>
     <img class='evolution-img' src="${pokemonImage}" alt="${formattedName}" />
@@ -156,11 +248,20 @@ function createEvolutionDiv(formattedName, pokemonData) {
   return pokemonDiv;
 }
 
+/**
+ * Loads the evolution chain for a Pokémon.
+ * @param {number} pokemonId - The ID of the Pokémon.
+ * @returns {void}
+ */
 async function loadEvolution(pokemonId) {
   await fetchEvolution(pokemonId);
 }
 
-//Pokemon-Category-Moves
+/**
+ * Generates HTML for displaying the moves of a Pokémon.
+ * @param {Object} pokemonData - The data of the Pokémon.
+ * @returns {string} - The HTML string representing the moves.
+ */
 function generatePokemonMoves(pokemonData) {
   let moves = pokemonData.moves;
   let movesHTML = "<div>";
@@ -170,12 +271,21 @@ function generatePokemonMoves(pokemonData) {
   return movesHTML;
 }
 
+/**
+ * Displays the Pokémon moves on the page.
+ * @param {string} content - The HTML content representing the moves.
+ * @returns {void}
+ */
 function displayPokemonMoves(content) {
   let categoryContent = document.getElementById("category-content");
   categoryContent.innerHTML = `<div class='moves-container'>${content}</div>`;
 }
 
-//Pokemon-StatusValues for Chart
+/**
+ * Retrieves the base stats of a Pokémon for charting.
+ * @param {Object} pokemonData - The data of the Pokémon.
+ * @returns {void}
+ */
 function pokemonStatusValues(pokemonData) {
   let baseStats = pokemonData.stats;
   statusNames = [];
@@ -183,11 +293,15 @@ function pokemonStatusValues(pokemonData) {
   for (let i = 0; i < baseStats.length; i++) {
     statusNames.push(baseStats[i].stat.name);
     statusValues.push(baseStats[i].base_stat);
-  } 
+  }
   loadChart();
 }
 
-//Pokemon-Category-About
+/**
+ * Fetches the first category of a Pokémon and generates its HTML content.
+ * @param {number} pokemonId - The ID of the Pokémon.
+ * @returns {void}
+ */
 async function fetchFirstCategory(pokemonId) {
   const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}/`;
   const response = await fetch(url);
@@ -195,25 +309,48 @@ async function fetchFirstCategory(pokemonId) {
   const abilityUrl = pokemonData.abilities[0].ability.url;
   const abilityResponse = await fetch(abilityUrl);
   const abilityData = await abilityResponse.json();
- generateFirstCategory(pokemonData, abilityData);
+  generateFirstCategory(pokemonData, abilityData);
 }
 
+/**
+ * Generates HTML content for the first category of a Pokémon.
+ * @param {Object} pokemonData - The data of the Pokémon.
+ * @param {Object} abilityData - The data of the Pokémon's ability.
+ * @returns {void}
+ */
 function generateFirstCategory(pokemonData, abilityData) {
-  let pokeAbility = abilityData.name.charAt(0).toUpperCase() + abilityData.name.slice(1).toLowerCase();
+  let pokeAbility =
+    abilityData.name.charAt(0).toUpperCase() +
+    abilityData.name.slice(1).toLowerCase();
   let pokeAbilityEffect = abilityData.effect_entries[0].short_effect;
   let height = pokemonData.height;
   let weight = pokemonData.weight;
-  let formattedWeight = weight/10;
-  document.getElementById('category-content').innerHTML = htmlContent= renderFirstCategory(pokeAbility, pokeAbilityEffect, height, formattedWeight);
+  let formattedWeight = weight / 10;
+  document.getElementById("category-content").innerHTML = htmlContent =
+    renderFirstCategory(
+      pokeAbility,
+      pokeAbilityEffect,
+      height,
+      formattedWeight
+    );
 }
 
-//functions for Pokemon Infos
+/**
+ * Formats the name of a Pokémon.
+ * @param {Object} pokemon - The data of the Pokémon.
+ * @returns {string} - The formatted name of the Pokémon.
+ */
 function formatPokemonName(pokemon) {
   pokemonName =
     pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1).toLowerCase();
   return pokemonName;
 }
 
+/**
+ * Checks if a Pokémon has a second type.
+ * @param {Object} pokemon - The data of the Pokémon.
+ * @returns {string|null} - The second type of the Pokémon or null if it has only one type.
+ */
 function checkSecondPokeType(pokemon) {
   if (pokemon.types.length > 1) {
     return pokemon.types[1]["type"].name;
@@ -222,19 +359,34 @@ function checkSecondPokeType(pokemon) {
   }
 }
 
+/**
+ * Retrieves the color of a Pokémon based on its type.
+ * @param {Array} types - The types of the Pokémon.
+ * @returns {string} - The color associated with the Pokémon's primary type.
+ */
 function getPokemonColor(types) {
   const primaryType = types[0].type.name;
   return typeColors[primaryType] || "gray";
 }
 
-//Search-field
+/**
+ * Filters Pokémon names based on the search term and displays them.
+ * @returns {void}
+ */
 async function filterNames() {
-  const searchTerm = document.getElementById("search-field").value.toLowerCase();
+  const searchTerm = document
+    .getElementById("search-field")
+    .value.toLowerCase();
   const pokedek = document.getElementById("pokedek");
   await filterAndDisplayPokemon(searchTerm, pokedek);
   toggleLoadMoreButton(searchTerm);
 }
 
+/**
+ * Toggles the visibility of the load more button based on the search term.
+ * @param {string} searchTerm - The search term entered by the user.
+ * @returns {void}
+ */
 async function toggleLoadMoreButton(searchTerm) {
   const loadMoreButton = document.getElementById("load-more-btn");
   if (searchTerm.trim() !== "") {
@@ -246,14 +398,20 @@ async function toggleLoadMoreButton(searchTerm) {
   }
 }
 
+/**
+ * Filters Pokémon based on the search term and displays them.
+ * @param {string} searchTerm - The search term entered by the user.
+ * @param {HTMLElement} pokedek - The container element for displaying Pokémon.
+ * @returns {void}
+ */
 async function filterAndDisplayPokemon(searchTerm, pokedek) {
-  const filteredPokemon = allPokemon.filter(pokemon => {
+  const filteredPokemon = allPokemon.filter((pokemon) => {
     return pokemon.name.toLowerCase().includes(searchTerm);
   });
   pokedek.innerHTML = "";
   let counter = 0;
   for (const pokemon of filteredPokemon) {
-    if (counter >= 20) break; // Limit
+    if (counter >= 20) break;
     await getPokemonInfos(`https://pokeapi.co/api/v2/pokemon/${pokemon.id}`);
     counter++;
   }
